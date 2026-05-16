c9b7a51d44bbb995acbe84470296ef9a2e1da21d
Andrey Piroženok <a.pirozenoks@dynatech.lv>
2026-05-16 12:55:28 +0300

    feat: game manager

--- scenes/game_manager/game_manager.gd
+++ scenes/game_manager/game_manager.gd
@@ -6,6 +6,10 @@ extends Node
 
 var _total_enemy_cost: int = 0
 
+var _event_queue: Array[TimedEvent] = []
+var _active_enemies: int = 0
+var _level_counter: int = 0 # first level is 1 not 0
+
 const LEVEL_TIME: float = 120.0
 const MAX_BIG_SPAWNS: int = 5
 const BIG_SPAWN_COST: int = 10
@@ -25,18 +29,51 @@ func _ready() -> void:
 		_total_enemy_cost += enemy.cost
 
 
-	var asd := _gen_level_spec(200)
-	for enemy in asd.enemies:
-		print("enemy", enemy.cost)
-
-	for tide in asd.tides:
-		print("TIDE")
-		for enemy in tide:
-			print("enemy", enemy.cost)
+	_next_level()
 
 
-func start() -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	var now := Time.get_ticks_usec()
+	while not _event_queue.is_empty() and _event_queue.back().t <= now:
+		_process_event(_event_queue.pop_back())
+		_try_finish_level()
+
+
+func _process_event(ev: TimedEvent) -> void:
+	var m: EnemyMother = EnemyMother.get_instance()
+	if ev.spawn != null:
+		var inst: Node2D = ev.spawn.scene.instantiate()
+		inst.connect_death_signal(_on_enemy_death)
+		m.single_spawn(inst)
+
+	if ev.tide.is_empty():
+		var insts: Array[Node2D] = []
+
+		for p: EnemySpec in ev.tide:
+			var inst: Node2D = p.scene.instantiate()
+			inst.connect_death_signal(_on_enemy_death)
+
+		m.tide_spawn(insts)
+
+
+func _on_enemy_death() -> void:
+	_active_enemies -= 1
+	_try_finish_level()
+
+
+func _try_finish_level() -> void:
+	if _active_enemies > 0 or not _event_queue.is_empty():
+		return
+	
+	_next_level()
+
+
+func _next_level() -> void:
+	_level_counter += 1
+
+	var balance := ceili(remap(_level_counter, 1, 10, 50, 1000))
+	var spec := _gen_level_spec(balance)
+	_execute_level_spec(spec)
 
 
 func _gen_level_spec(budget: int) -> LevelSpec:
@@ -107,6 +144,43 @@ func _pick_enemy(budget: int) -> EnemySpec:
 	return enemies.back()
 
 
+func _execute_level_spec(spec: LevelSpec) -> void:
+	var start := Time.get_ticks_usec() + 1000000 # in 1 sec
+
+	_active_enemies = 0
+	for i: int in spec.enemies.size():
+		var delay: int = roundi(remap(i, 0, spec.enemies.size() - 1, 0, ceili(LEVEL_TIME * 1e6)))
+
+		_active_enemies += 1
+		_event_queue.push_back(TimedEvent.single(start+delay, spec.enemies[i]))
+
+	for i: int in spec.tides.size():
+		var delay: int = roundi(remap(i, 0, spec.tides.size() - 1, ceili(LEVEL_TIME * 0.3 * 1e6), ceili(LEVEL_TIME * 1e6)))
+
+		_active_enemies += spec.tides[i].size()
+		_event_queue.push_back(TimedEvent.many(start+delay, spec.tides[i]))
+	
+
+
 class LevelSpec:
 	var enemies: Array[EnemySpec] = []
 	var tides: Array[Array] = [] # Array[Array[EnemySpec]]
+
+
+class TimedEvent:
+	var t: int
+	var spawn: EnemySpec
+	var tide: Array[EnemySpec]
+
+	static func single(time: int, v: EnemySpec) -> TimedEvent:
+		var te := TimedEvent.new()
+		te.t = time
+		te.spawn = v
+		return te
+
+	static func many(time: int, v: Array[EnemySpec]) -> TimedEvent:
+		var te := TimedEvent.new()
+		te.t = time
+		te.tide =  v
+		return te
+	
