b79d224392b0a45b1ca57cb19c85282be1dae522
TrueHopolok <vadim.truhanov@inbox.lv>
2026-05-16 14:10:42 +0300

    feat: added Twin Enemy

---
+++ scenes/enemy/advanced/enemy_twin.gd
@@ -0,0 +1,38 @@
+class_name EnemyTwin
+extends Enemy
+
+
+@export var _stun_duration: float = 3.0
+
+var twin: EnemyTwin
+var _stun_left: float = 0.0
+
+
+func _ready() -> void:
+	twin.died.connect(_die)
+
+
+func _physics_process(delta: float) -> void:
+	if stunned():
+		_stun_left = clampf(_stun_left - delta, 0.0, _stun_duration)
+	else:
+		super(delta)
+	queue_redraw()
+
+
+func _draw() -> void:
+	draw_line(to_local(global_position), to_local(twin.global_position), Color.WHITE)
+
+
+func take_damage() -> void:
+	if stunned() || health <= 0:
+		return
+	health -= 1
+	if health <= 0:
+		if twin.stunned():
+			_die()
+		_stun_left = _stun_duration
+
+
+func stunned() -> bool:
+	return _stun_left > 0.0
---
+++ scenes/enemy/advanced/enemy_twin_spawner.gd
@@ -0,0 +1,29 @@
+class_name EnemyTwinSpawner
+extends Node2D
+
+
+@export var _twin_1: PackedScene
+@export var _twin_2: PackedScene
+@export var _spawn_radius: float = 20.0
+@onready var _inst_1: EnemyTwin = _twin_1.instantiate()
+@onready var _inst_2: EnemyTwin = _twin_2.instantiate()
+
+func _ready() -> void:
+	var x: float = randf_range(0, _spawn_radius)
+	var y: float = sqrt(_spawn_radius * _spawn_radius - x * x)
+	_inst_1.position = Vector2(x, y)
+	_inst_2.position = Vector2(-x, -y)
+	_inst_1.twin = _inst_2
+	_inst_2.twin = _inst_1
+	get_parent().add_child.call_deferred(_inst_1)
+	get_parent().add_child.call_deferred(_inst_2)
+	queue_free()
+
+
+func connect_death_signal(f: Callable) -> void:
+	_twin_1.died.connect(f)
+	_twin_2.died.connect(f)
+
+
+func get_enemy_amount() -> int:
+	return 2
