80777098554adbc2cec52a12bf9e7b739407c0b1
TrueHopolok <vadim.truhanov@inbox.lv>
2026-05-16 12:21:05 +0300

    feat: added basic Enemy

---
+++ scenes/enemy/enemy.gd
@@ -0,0 +1,32 @@
+class_name Enemy
+extends CharacterBody2D
+
+
+signal died()
+
+const GROUP: StringName = &"Enemy"
+const TARGET: Vector2 = Vector2(320, 180)
+
+@export var health: int = 1
+@export var speed: float = 5.0
+
+
+func _physics_process(_delta: float) -> void:
+	velocity = global_position.direction_to(TARGET) * speed
+	move_and_slide()
+
+
+func _die() -> void:
+	died.emit()
+
+
+func connect_death_signal(f: Callable) -> void:
+	died.connect(f)
+
+
+func take_damage() -> void:
+	if health <= 0:
+		return
+	health -= 1
+	if health <= 0:
+		_die()
