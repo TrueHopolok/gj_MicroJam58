3e0cd187de4eae3db8d95ab5a282e2af2e1399d5
kitaminka <kit4minka@gmail.com>
2026-05-16 21:38:33 +0300

    feat: Add crab sprite

--- scenes/enemy/advanced/enemy_rotate.gd
+++ scenes/enemy/advanced/enemy_rotate.gd
@@ -11,4 +11,8 @@ func _physics_process(_delta: float) -> void:
 	var p: Vector2 = global_position.direction_to(TARGET)
 	var d: Vector2 = Vector2(p.y, -p.x) if _counter_clockwise else Vector2(-p.y, p.x)
 	velocity = p * speed + d * _rotation_strength
+	
 	move_and_slide()
+
+func _process(_delta: float) -> void:
+	_sprite.look_at(TARGET)
