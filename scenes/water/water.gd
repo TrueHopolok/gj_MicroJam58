7355c58009ad16e88df0a8fec8e1b10cda264c89
wailer <andrypirk@gmail.com>
2026-05-17 12:48:27 +0300

    fix: water in main menu

--- scenes/water/water.gd
+++ scenes/water/water.gd
@@ -1,5 +1,17 @@
 extends Node
 
 
+@export var close_hole: bool = false
+
+
+func _ready() -> void:
+	if close_hole:
+		$AnimationPlayer.queue_free()
+		var wr := $SubViewport/WaterRect as CanvasItem
+		var mat := (wr.material as ShaderMaterial)
+		mat.set_shader_parameter("hole_rad", 0.0)
+		mat.set_shader_parameter("hole_foam_thickness", 0.0)
+
+
 func play_spawn_anim() -> void:
 	$AnimationPlayer.play(&"move_in")
