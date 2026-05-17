007b86bb27085ef0df260f21dfaf6778bdcb9215
TrueHopolok <vadim.truhanov@inbox.lv>
2026-05-17 14:44:47 +0300

    added music players filters

--- scenes/audio/gameplay_audio_player/gameplay_audio_player.gd
+++ scenes/audio/gameplay_audio_player/gameplay_audio_player.gd
@@ -3,9 +3,22 @@ extends AudioStreamPlayer
 
 @export var loop_stream: AudioStream
 
+@onready var _bus_idx: int = AudioServer.get_bus_index(&'Music')
+@onready var _effect_idx: int = 0
+
 
 func _ready() -> void:
 	finished.connect(func() -> void:
 		stream = loop_stream
 		play()
 	)
+
+
+func _physics_process(_delta: float) -> void:
+	if OS.get_name() == "Web":
+		if get_tree().paused:
+			volume_db = -10
+		else:
+			volume_db = 0
+	else:
+		AudioServer.set_bus_effect_enabled(_bus_idx, _effect_idx, get_tree().paused)
