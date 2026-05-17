d716ae8855f84e5857b26fa779b7451caf476529
TrueHopolok <vadim.truhanov@inbox.lv>
2026-05-17 20:57:37 +0300

    fixup! added tutorial skelet

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -4,11 +4,13 @@ extends CanvasLayer
 
 @export var hints: Dictionary[int, String]
 
+@onready var label: Label = $Hint
+
 
 func on_level_switched(level_num: int) -> void:
 	if level_num in hints.keys():
 		_show_hint(hints[level_num])
 
 
-func _show_hint(_hint: String) -> void:
+func _show_hint(hint: String) -> void:
 	pass
