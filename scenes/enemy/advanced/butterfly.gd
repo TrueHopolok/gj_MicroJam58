ae70bf6fd2339168fadc6b36395e64d2cefcfd49
Zhizhoid <misha2006.mk@gmail.com>
2026-05-17 12:12:54 +0300

    refactor sfx, add heal sfx

--- scenes/enemy/advanced/butterfly.gd
+++ scenes/enemy/advanced/butterfly.gd
@@ -4,6 +4,7 @@ extends Enemy
 
 @export var heal: int = 10
 
+@onready var _heal_sfx_player: AudioStreamPlayer = GameplaySfxPlayer.get_audio_stream_player("CastleHeal")
 
 func _die() -> void:
 	super()
@@ -13,4 +14,5 @@ func _die() -> void:
 func target_reached() -> int:
 	queue_free()
 	died.emit(score)
+	_heal_sfx_player.play()
 	return -heal
